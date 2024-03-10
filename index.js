e79de34f0b5bbd2e08b8c72ee6106c8d7db92bd7
Annas03 <baigannas9@gmail.com>
2024-03-10 22:17:23 +0500

    implement cron job

--- index.js
+++ index.js
@@ -1,39 +1,92 @@
 "use strict";
 
-const { createServer } = require("@lhci/server");
-const cron = require("node-cron");
-const { exec } = require("child_process");
+// const { createServer } = require("@lhci/server");
+// const cron = require("node-cron");
+// const { exec } = require("child_process");
 
-async function runLhci() {
-  try {
-    // Example (replace with your storage implementation):
+const express = require('express');
+const lhci = require('@lhci/server');
 
-    exec(
-      "lhci autorun --config=./lighthouerc.cjs --collect.url=https://ang-todotask.netlify.app/ --collect.numberOfRuns=1 --upload.target=lhci --upload.serverBaseUrl=https://lighthouse-server-production-8084.up.railway.app --upload.token=0ac98669-2c1f-4b1d-99c2-4acd8f163e45",
-      (error, stdout) => {
-        if (error) {
-          console.log("Error", error.message);
-          return;
+(async () => {
+  const app = express();
+  const { app: lhciApp } = await lhci.createApp({
+    storage: {
+      storageMethod: "sql",
+      sqlDialect: "postgres",
+      sqlConnectionSsl: true,
+      sqlConnectionUrl:
+        "postgresql://postgres:*BEc52G5Abf2FFbaG1c-c*ee3GdA66ae@viaduct.proxy.rlwy.net:19632/railway",
+    },
+    psiCollectCron: {
+      psiApiKey: "AIzaSyALh8C-UWtyiGN3MvJPSfcjo2QkkZ2FXwo",
+      sites: [
+        {
+          urls: ["https://ang-todotask.netlify.app/"],
+          schedule: "*/5 * * * *",
+          projectSlug: "lightouse-server"
         }
-        console.log("Output", stdout);
-      }
-    );
-  } catch (error) {
-    console.error("Error running LHCI:", error);
-  }
-}
+      ]
+    }
+  });
 
-console.log("Starting server...");
-createServer({
-  port: process.env.PORT,
-  storage: {
-    storageMethod: "sql",
-    sqlDialect: "postgres",
-    sqlConnectionSsl: true,
-    sqlConnectionUrl:
-      "postgresql://postgres:*BEc52G5Abf2FFbaG1c-c*ee3GdA66ae@viaduct.proxy.rlwy.net:19632/railway",
-  },
-}).then(({ port }) => console.log("Listening on port", port));
+  app.use((req, res, next) => handleCustomAuthentication(req, res, next));
+  app.use(lhciApp);
+  app.listen();
+})();
 
-cron.schedule("*/2 * * * *", runLhci); // Runs at every 5 minute
-console.log("LHCI cron job started!");
+
+// const auditCommand = 'lhci collect --collect.url=https://ang-todotask.netlify.app/ --collect.numberOfRuns=1';
+
+// // Command to upload report to LHCI server
+// const uploadCommand = 'lhci upload --target=lhci --serverBaseUrl=https://lighthouse-server-production-8084.up.railway.app --token=0ac98669-2c1f-4b1d-99c2-4acd8f163e45';
+
+// // Function to execute shell commands
+// function runCommand(command) {
+//   return new Promise((resolve, reject) => {
+//     exec(command, (error, stdout, stderr) => {
+//       if (error) {
+//         console.error(`exec error: ${error}`);
+//         reject(error);
+//         return;
+//       }
+//       console.log(`stdout: ${stdout}`);
+//       console.error(`stderr: ${stderr}`);
+//       resolve();
+//     });
+//   });
+// }
+
+// async function runLhci() {
+//   try {
+//     // Run Lighthouse CI audit
+//     runCommand(auditCommand)
+//       .then(() => {
+//         console.log('Lighthouse CI audit completed successfully.');
+//         // Upload report to LHCI server
+//         return runCommand(uploadCommand);
+//       })
+//       .then(() => {
+//         console.log('Report uploaded to LHCI server.');
+//       })
+//       .catch(error => {
+//         console.error('An error occurred:', error);
+//       });
+//   } catch (error) {
+//     console.error("Error running LHCI:", error);
+//   }
+// }
+
+// console.log("Starting server...");
+// createServer({
+//   port: process.env.PORT,
+//   storage: {
+//     storageMethod: "sql",
+//     sqlDialect: "postgres",
+//     sqlConnectionSsl: true,
+//     sqlConnectionUrl:
+//       "postgresql://postgres:*BEc52G5Abf2FFbaG1c-c*ee3GdA66ae@viaduct.proxy.rlwy.net:19632/railway",
+//   },
+// }).then(({ port }) => console.log("Listening on port", port));
+
+// cron.schedule("* * * * *", runLhci); // Runs at every 5 minute
+// console.log("LHCI cron job started!");
